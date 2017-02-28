c29584a83e4f2bb279a4d544129bef7b51399a2c
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-02-28 14:57:01 -0500

    Updated from Brython Server: 2/28/2017 2:57:00 PM

--- mightbeontosomething.py
+++ mightbeontosomething.py
@@ -2,13 +2,24 @@ width =  10 #input("What is the width?")
 height = 8 #input("What is the height?")
 
 mylist = range(1, int(width) +1)
-n = 5
-
+n = 0
+end1="  "
  
 #loop
 while n <= int(height):
-      n = n + 1
-      for b in mylist:
-        b = b * n
-        for b in mylist:
-            print(, end="  ")        #, end = " ")
+    n = n + 1
+    for b in mylist:
+        if mylist[3] >= 10:
+            print(b * n, end=end1)
+        else:
+            print(b * n, end=" ")
+    print("")
+    
+    
+    """
+    if endnumber >=10:
+        then end= " "
+        
+        """
+        
+        
